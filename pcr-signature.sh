b6b4f3e73cfb572448fa0b28b962af4fcc817e5f
Alberto Planas <aplanas@suse.com>
2024-03-12 16:06:09 +0100

    Suppor GRUB2 configurations
    Signed-off-by: Alberto Planas <aplanas@suse.com>

--- pcr-signature.sh
+++ pcr-signature.sh
@@ -3,7 +3,10 @@
 MNT="/tmp/pcr-signature"
 # Maybe a better place is loader/credentials
 SYSTEMD="EFI/systemd"
+OPENSUSE="EFI/opensuse"
+SIGNATURES=""
 VENDOR="4a67b082-0a4c-41cf-b6c7-440b29bb8c4f"
+# If GRUB2 is used, bli.mod needs to be loaded
 EFIVAR="/sys/firmware/efi/efivars/LoaderDevicePartUUID-$VENDOR"
 
 cleanup()
@@ -20,7 +23,13 @@ is_mount() {
 mount_esp() {
     [ -e "$EFIVAR" ] || return 0
     mount "$DEV" "$MNT"
-    [ -e "${MNT}/${SYSTEMD}" ] && return 0
+    if [ -e "${MNT}/${SYSTEMD}" ]; then
+	SIGNATURES="$SYSTEMD"
+	return 0
+    elif [ -e "${MNT}/${OPENSUSE}" ]; then
+	SIGNATURES="$OPENSUSE"
+	return 0
+    fi
     umount "$MNT"
 }
 
@@ -42,13 +51,13 @@ mkdir -p "$MNT"
 mount_esp
 
 if is_mount "$MNT"; then
-    if [ -e "${MNT}/${SYSTEMD}/pcrlock.json" ]; then
+    if [ -e "${MNT}/${SIGNATURES}/pcrlock.json" ]; then
 	mkdir -p /var/lib/systemd
-	cp "${MNT}/${SYSTEMD}/pcrlock.json" /var/lib/systemd
-    elif [ -e "${MNT}/${SYSTEMD}/tpm2-pcr-signature.json" ] && [ -e "${MNT}/${SYSTEMD}/tpm2-pcr-public-key.pem" ]; then
+	cp "${MNT}/${SIGNATURES}/pcrlock.json" /var/lib/systemd
+    elif [ -e "${MNT}/${SIGNATURES}/tpm2-pcr-signature.json" ] && [ -e "${MNT}/${SIGNATURES}/tpm2-pcr-public-key.pem" ]; then
 	mkdir -p /etc/systemd
-	cp "${MNT}/${SYSTEMD}/tpm2-pcr-signature.json" /etc/systemd
-	cp "${MNT}/${SYSTEMD}/tpm2-pcr-public-key.pem" /etc/systemd
+	cp "${MNT}/${SIGNATURES}/tpm2-pcr-signature.json" /etc/systemd
+	cp "${MNT}/${SIGNATURES}/tpm2-pcr-public-key.pem" /etc/systemd
     fi
 fi
 
