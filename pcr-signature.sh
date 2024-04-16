ae07fc611de1884207614125533482477e9e2f8b
Alberto Planas <aplanas@suse.com>
2024-04-16 19:19:21 +0200

    Do not hard fail when error in mount
    Signed-off-by: Alberto Planas <aplanas@suse.com>

--- pcr-signature.sh
+++ pcr-signature.sh
@@ -7,8 +7,8 @@ EFIVAR="/sys/firmware/efi/efivars/LoaderDevicePartUUID-4a67b082-0a4c-41cf-b6c7-4
 [ -e "$EFIVAR" ] || exit 0
 
 if [ -e "/var/lib/systemd/pcrlock.json" ] || [ -e "/etc/systemd/tpm2-pcr-signature.json" ]; then
-	# Already ran?
-	exit 0
+    echo "pcr-signature: signature file already present"
+    exit 0
 fi
 
 # Read the value of the EFI variable, that contains a header and
@@ -19,24 +19,30 @@ MNT="$(mktemp -d)"
 
 cleanup()
 {
-	if mountpoint -q "$MNT"; then
-		umount "$MNT"
-	fi
-	rmdir "$MNT"
+    if mountpoint -q "$MNT"; then
+	umount "$MNT" || {
+	    echo "pcr-signature: unable to umount ESP"
+	    exit 0
+	}
+    fi
+    rmdir "$MNT"
 }
 trap cleanup EXIT
 
-mount -o ro "$DEV" "$MNT"
+mount -o ro "$DEV" "$MNT" || {
+    echo "pcr-signature: unable to mount ESP"
+    exit 0
+}
 
 for location in "${MNT}/EFI/systemd" "${MNT}/EFI/opensuse"; do
-	if [ -e "${location}/pcrlock.json" ]; then
-		mkdir -p /var/lib/systemd
-		cp "${location}/pcrlock.json" /var/lib/systemd
-		break
-	elif [ -e "${location}/tpm2-pcr-signature.json" ] && [ -e "${location}/tpm2-pcr-public-key.pem" ]; then
-		mkdir -p /etc/systemd
-		cp "${location}/tpm2-pcr-signature.json" /etc/systemd
-		cp "${location}/tpm2-pcr-public-key.pem" /etc/systemd
-		break
-	fi
+    if [ -e "${location}/pcrlock.json" ]; then
+	mkdir -p /var/lib/systemd
+	cp "${location}/pcrlock.json" /var/lib/systemd
+	break
+    elif [ -e "${location}/tpm2-pcr-signature.json" ] && [ -e "${location}/tpm2-pcr-public-key.pem" ]; then
+	mkdir -p /etc/systemd
+	cp "${location}/tpm2-pcr-signature.json" /etc/systemd
+	cp "${location}/tpm2-pcr-public-key.pem" /etc/systemd
+	break
+    fi
 done
